df4ae3f46f74279277e9b182a9ebc15b6e74e34c
Francesco Tizzani <francesco@tizzani.org>
2025-11-15 08:36:09 +0000

    Fixes in Replit

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-c0c2aabf'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.c1jliock53g"
+    "revision": "0.6huptlfa01"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
